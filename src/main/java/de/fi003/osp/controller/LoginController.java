2c2e7e5f31bbcf59f74dfa89463f0862c64244b8
BenCoepp <bencoepp@gmail.com>
2022-11-14 11:02:03 +0100

    added login page

---
+++ src/main/java/de/fi003/osp/controller/LoginController.java
@@ -0,0 +1,17 @@
+package de.fi003.osp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @GetMapping("")
+    public String main(Model model) {
+        model.addAttribute("pageTitle","Login - Application");
+        return "login";
+    }
+}
