be7bc4dc39c28cee0412ac344f9ef1eb184e86f6
BenCoepp <bencoepp@gmail.com>
2022-11-14 10:42:10 +0100

    first commit

---
+++ src/test/java/de/fi003/osp/OsbApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fi003.osp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
