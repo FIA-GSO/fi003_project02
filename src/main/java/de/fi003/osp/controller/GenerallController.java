9b45e6ef1c1a63d0a9ad500a353fdbd0d0fcc5a0
BenCoepp <bencoepp@gmail.com>
2022-11-14 15:18:26 +0100

    update pos and ref

--- src/main/java/de/fi003/osp/controller/GenerallController.java
+++ src/main/java/de/fi003/osp/controller/GenerallController.java
@@ -1,5 +1,7 @@
 package de.fi003.osp.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +20,11 @@ public class GenerallController {
     @GetMapping("/class")
     public String getClassOverview(Model model) {
         model.addAttribute("pageTitle"," Klassenübersicht - Application");
+        ArrayList<String> classList = new ArrayList<>();
+        classList.add("FI001");
+        classList.add("FI002");
+        classList.add("FI003");
+        model.addAttribute("list", classList);
         return "class_select";
     }
 }
