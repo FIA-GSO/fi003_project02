d78c6384d4a41b203ba24e9317dd0fdc5c4afdf7
BenCoepp <bencoepp@gmail.com>
2022-11-16 15:01:06 +0100

    update id and selcetor for js

--- src/main/resources/static/js/grade_create.js
+++ src/main/resources/static/js/grade_create.js
@@ -6,8 +6,11 @@ $(document).ready(function() {
         
         $('#loading').hide();
     }); 
-
-    for (let index = 0; index < 28; index++) {
-        $("#gradeInput").append('<input type="text" class="form-control" aria-label="Text input" id="inputField' + index + '">');
+    var elementList = $('[id=gradeInput]');
+    for (let index = 0; index < elementList.length; index++) {
+        const element = elementList[index];
+        for (let index = 0; index < 28; index++) {
+            $(element).append('<input type="text" class="form-control" aria-label="Text input" id="inputField' + index + '">');
+        }
     }
 });
